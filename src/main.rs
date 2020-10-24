73c1eceb8a26ab07e9ffaa786ff063f14e8c7895
Daiderd Jordan <daiderd@gmail.com>
2020-10-24 10:37:20 +0200

    add os detection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::io;
+use std::io::{BufRead, BufReader};
+use std::fs::File;
+
+fn parse_line(line: String) -> Option<String> {
+    if line.starts_with("NAME=") {
+        Some(line[5..].into())
+    } else {
+        None
+    }
+}
+
+fn os_name() -> io::Result<String> {
+    let os_release = File::open("/etc/os-release")?;
+    for line in BufReader::new(os_release).lines() {
+        if let Some(name) = parse_line(line?) {
+            return Ok(name);
+        }
+    }
+    Err(io::Error::new(io::ErrorKind::NotFound, "OS name not detected"))
+}
+
 fn main() {
-    println!("Hello, world!");
+    let greeting = match os_name() {
+        Ok(name) => name,
+        Err(_) => "world".into(),
+    };
+    println!("Hello, {}!", greeting);
 }
